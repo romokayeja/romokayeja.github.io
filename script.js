b0e76cb5b0cb213b6aed79cc0309ffe4f3f37248
romokayeja <romokayeja@outlook.com>
2024-12-14 17:39:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-function toggleMenu() {
-    const menu = document.getElementById('macosMenu');
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
-}
-
-function openAbout() {
-    const aboutWindow = document.getElementById('aboutWindow');
-    aboutWindow.style.display = 'block';
-}
-
 function closeAboutWindow() {
     const aboutWindow = document.getElementById('aboutWindow');
     aboutWindow.style.display = 'none';
@@ -26,33 +16,3 @@ function maximizeWindow(windowId) {
     windowElement.style.left = '0';
     windowElement.style.transform = 'none';
 }
-
-function closeTab() {
-    window.close();
-}
-
-function startMove(event) {
-    const windowElement = event.target.closest('.window');
-    let shiftX = event.clientX - windowElement.getBoundingClientRect().left;
-    let shiftY = event.clientY - windowElement.getBoundingClientRect().top;
-
-    function moveAt(pageX, pageY) {
-        windowElement.style.left = pageX - shiftX + 'px';
-        windowElement.style.top = pageY - shiftY + 'px';
-    }
-
-    function onMouseMove(event) {
-        moveAt(event.pageX, event.pageY);
-    }
-
-    document.addEventListener('mousemove', onMouseMove);
-
-    windowElement.onmouseup = function() {
-        document.removeEventListener('mousemove', onMouseMove);
-        windowElement.onmouseup = null;
-    };
-}
-
-document.querySelectorAll('.window').forEach(windowElement => {
-    windowElement.addEventListener('mousedown', startMove);
-});
